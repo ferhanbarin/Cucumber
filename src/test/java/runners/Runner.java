ba6e4b5e14317d871bbd31cf637133e7522c5666
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-11-04 19:39:08 +0200

    Cucumber

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = "stepdefinitions", tags = "@Editor", dryRun = true)
+@CucumberOptions(plugin={"html:target//cucumber-reports.html"}, features = "src/test/resources/features", glue = "stepdefinitions", tags = "@Editor", dryRun = true)
 
 // dryRun = true dedigimizde testi calistirmadan bize eksik adimi verir.
 // dryRun = false yaparsak testi calistirir, eksik bir adimla karsilasirsa test failed olur ve bize eksik adimi raporlar.
